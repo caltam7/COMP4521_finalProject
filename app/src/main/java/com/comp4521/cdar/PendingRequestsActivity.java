262707dde671261d7a83605d5d48d22c24acf973
caltam7 <101660165+caltam7@users.noreply.github.com>
2023-05-11 00:29:10 +0800

    Created PendingRequestsActivity

---
+++ app/src/main/java/com/comp4521/cdar/PendingRequestsActivity.java
@@ -0,0 +1,14 @@
+package com.comp4521.cdar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PendingRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_requests);
+    }
+}
